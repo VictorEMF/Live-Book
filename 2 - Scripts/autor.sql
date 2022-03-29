094bd7d528acdb148f08cbcf119f89c1a9365513
Victor Emanuel <70483515+VictorEMF@users.noreply.github.com>
2022-03-29 16:18:07 -0300

    Script autor

---
+++ 2 - Scripts/autor.sql
@@ -0,0 +1,22 @@
+create table autor(
+    cod_autor int primary key,
+    nome varchar(100),
+    pais varchar(100)
+);
+
+
+insert into autor (cod_autor, nome, pais) values (1, 'Gregg Hurwitz', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (2, 'James Dashner', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (3, 'Sarah J. Maas', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (4, 'Oliver Bowden', 'Inglaterra');
+insert into autor (cod_autor, nome, pais) values (5, 'Rick Riordan', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (6, 'Neil Gaiman', 'Inglaterra');
+insert into autor (cod_autor, nome, pais) values (7, 'Katie Coyle', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (8, 'Madeleine L''engle', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (9, 'Douglas Adams', 'Inglaterra');
+insert into autor (cod_autor, nome, pais) values (10, 'Carina Rissi', 'Brasil');
+insert into autor (cod_autor, nome, pais) values (11, 'Jamie McGuire', 'Estados Unidos');
+insert into autor (cod_autor, nome, pais) values (12, 'Guillermo del Toro', 'México');
+insert into autor (cod_autor, nome, pais) values (13, 'Philip Pullman', 'Inglaterra');
+insert into autor (cod_autor, nome, pais) values (14, 'Stieg Larsson', 'Suécia');
+insert into autor (cod_autor, nome, pais) values (15, 'David Lagercrantz', 'Suécia');
