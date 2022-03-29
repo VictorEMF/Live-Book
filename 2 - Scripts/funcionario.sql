81dcd5407d91e3416ee56b8a803c0db91e2f3338
Victor Emanuel <70483515+VictorEMF@users.noreply.github.com>
2022-03-29 16:18:50 -0300

    Script funcionario

---
+++ 2 - Scripts/funcionario.sql
@@ -0,0 +1,17 @@
+create table funcionario(
+    cod_funcionario int primary key,
+    nome varchar(100),
+    sexo char(1),
+    cargo varchar(20)
+);
+
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (1, 'Bethany','M', 'Gerente');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (2, 'Jeane','M', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (3, 'Forster','M', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (4, 'Fitz','F', 'Gerente');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (5, 'Maure','M', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (6, 'Veradis','F', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (7, 'Redd','M', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (8, 'Lenette','F', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (9, 'Melodee','F', 'Recepcionista');
+insert into funcionario (cod_funcionario, nome, sexo, cargo) values (10, 'Mari','F', 'Gerente');
